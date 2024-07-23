8c4a79687b07094f5b252cfb87773353e5a35ce0
e502692 <daniel.schreiber@sbb.ch>
2024-07-23 12:01:21 +0200

    add ngx-scrollbar and reproduce error with patched polyfill

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {NgForOf} from "@angular/common";
+import {NgScrollbar} from "ngx-scrollbar";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, NgForOf, NgScrollbar],
   template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+    <h1>Reproduction for ngx-scrollbar polyfill error</h1>
+    <p>Use Firefox, hit "toggle" and observe error in js console..</p>
+    <button (click)="showScrollContent = !showScrollContent">toggle</button>
+    <ng-scrollbar style="height: 300px; width: 400px; border: 2px green solid">
+      @if (showScrollContent) {
+        <div *ngFor="let i of [].constructor(100); let idx = index">Item {{ idx }}</div>
+      }
+    </ng-scrollbar>
   `,
   styles: [],
 })
 export class AppComponent {
-  title = 'ngx-scrollbar-reproduce-polyfill-error';
+  showScrollContent = true;
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideScrollbarPolyfill} from "ngx-scrollbar";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+    providers: [
+        provideZoneChangeDetection({eventCoalescing: true}),
+        provideScrollbarPolyfill('https://cdn.statically.io/gist/MurhafSousli/c852b6a672069396953f06ddd4b64620/raw/0b2553be83108b96b442c7004b08ef57c9282bfe/scroll-timeline-lite.js')
+    ]
 };
